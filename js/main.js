370811da51983ff821b689f86648ba8009d84891
Eliasib García Martín <eliasib@MacBook-Pro-de-Eliasib.local>
2016-06-15 13:16:24 +0100

    First commit. Only GET requests allowed at the moment.

---
+++ js/main.js
@@ -0,0 +1,66 @@
+/**
+ * Created by eliasib on 14/6/16.
+ */
+
+// This var will store responses of requests resolved.
+window.results = [];
+
+(function(window) {
+    /**
+     * Function that initialize all Semantic UI components
+     */
+    function semanticInit() {
+
+        // Init dropdowns
+        $('.ui.dropdown')
+            .dropdown()
+        ;
+
+    }
+
+    /**
+     * Function that launches the request
+     */
+    function launchRequest() {
+        $('.loader').show();
+        var method = $('#method').text();
+        var url = $('#url').val();
+
+        $.ajax({
+            url: url,
+            method: method,
+            success: function(data, textStatus, jqXHR) {
+                $('#result-message').removeClass('red');
+                $('#result-message').addClass('ui message green');
+                $('#result-message-status').html(textStatus);
+
+                if (!window.results.push)
+                    window.results = [];
+                window.results.push(data);
+                $('#result-message-code').html('results[' + (window.results.length - 1) + ']');
+                $('#result-message-extra').show();
+                console.info('Results stored on results[' + (window.results.length - 1) + ']');
+
+                $('.loader').hide();
+                $('#result-message').css('display','block');
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                $('#result-message').removeClass('green');
+                $('#result-message').addClass('ui message red');
+                $('#result-message-status').html(textStatus);
+
+                $('#result-message-extra').hide();
+
+                $('.loader').hide();
+                $('#result-message').css('display','block');
+            }
+        });
+    }
+
+    // Init Semantic UI components
+    semanticInit();
+
+    // Event binding
+    $('#launch-request').click(launchRequest);
+
+})(window);
